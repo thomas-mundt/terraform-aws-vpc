07d2ddb6abfabaa679f50a6b7c0c7d1af17b0a66
Thomas Mundt <mundt_thomas@yahoo.de>
2024-12-17 13:29:41 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
+output "vpc_id" {
+  description = "The ID of the created VPC."
+  value       = aws_vpc.main.id
+}
 
+output "vpc_arn" {
+  description = "The ARN of the created VPC."
+  value       = aws_vpc.main.arn
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the created VPC."
+  value       = aws_vpc.main.cidr_block
+}
